b53dd8ea8f1e2ac6808645f8da8bed59b065d7b4
Paul <paulbigben@gmail.com>
2024-10-09 10:18:31 +0200

    P4 begin

---
+++ src/main/java/dp/assignments/ovchip/domain/OVChipkaart.java
@@ -0,0 +1,75 @@
+package dp.assignments.ovchip.domain;
+
+import javax.persistence.*;
+import java.util.Date;
+
+public class OVChipkaart {
+
+    private int kaartNummer;
+    private Date geldigTot;
+    private int klasse;
+    private double saldo;
+    private Reiziger reiziger;
+
+    public OVChipkaart(int kaartNummer, Date geldigTot, int klasse, double saldo, Reiziger reiziger) {
+        this.kaartNummer = kaartNummer;
+        this.geldigTot = geldigTot;
+        this.klasse = klasse;
+        this.saldo = saldo;
+        this.reiziger = reiziger;
+    }
+
+    public OVChipkaart() {
+
+    }
+
+    public int getKaartNummer() {
+        return kaartNummer;
+    }
+
+    public void setKaartNummer(int kaartNummer) {
+        this.kaartNummer = kaartNummer;
+    }
+
+    public Date getGeldigTot() {
+        return geldigTot;
+    }
+
+    public void setGeldigTot(Date geldigTot) {
+        this.geldigTot = geldigTot;
+    }
+
+    public int getKlasse() {
+        return klasse;
+    }
+
+    public void setKlasse(int klasse) {
+        this.klasse = klasse;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public Reiziger getReiziger() {
+        return reiziger;
+    }
+
+    public void setReiziger(Reiziger reiziger) {
+        this.reiziger = reiziger;
+    }
+
+    @Override
+    public String toString() {
+        return "OVChipkaart: (" + kaartNummer +
+                ", geldig tot: " + geldigTot +
+                ", klasse: " + klasse +
+                ", saldo: " + saldo +
+                ", reiziger: " + (reiziger != null ? reiziger.toString() : "null") +
+                ')';
+    }
+}
